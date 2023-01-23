b73c85d7872ef7ad2ba0c019e41d0f64ae6ef5d8
Ian VF <ianvf03@gmail.com>
2023-01-23 13:12:14 +0100

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Apps2M on 23/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
